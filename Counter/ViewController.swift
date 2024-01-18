e405c508768dba0beafc21b573e4a39532f11c9a
alibi <mailan.alibi.97@yandex.kz>
2024-01-18 21:16:21 +0600

    добавлен label и button

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func minesAction(_ sender: Any) {
+        count = count + 1
+        label.text = "\(count)"
+    }
+    @IBAction func plusAction(_ sender: Any) {
+        count = count - 1
+        label.text = "\(count)"
+    }
+    
 }
 
