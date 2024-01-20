b547c22b993232724d6dd6e395ff1941df78562e
alibi <mailan.alibi.97@yandex.kz>
2024-01-20 23:16:01 +0600

    add textView

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -11,24 +11,39 @@ class ViewController: UIViewController {
 
     @IBOutlet var label: UILabel!
     var count = 0
+    @IBOutlet var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func minesAction(_ sender: Any) {
+    @IBAction func plusAction(_ sender: Any) {
         count = count + 1
         label.text = "\(count)"
+        textView.insertText("\(getNowDate()) значение изменено на +1\n")
     }
-    @IBAction func plusAction(_ sender: Any) {
+    @IBAction func minusAction(_ sender: Any) {
+        if (count == 0) {
+            textView.insertText("\(getNowDate()) попытка уменьшить значение счётчика ниже 0\n")
+            return
+        }
         count = count - 1
         label.text = "\(count)"
+        textView.insertText("\(getNowDate()) значение изменено на -1\n")
     }
     
     @IBAction func resetAction(_ sender: Any) {
         count = 0
         label.text = "\(count)"
-        
+        textView.insertText("\(getNowDate()): значение сброшено\n")
+    }
+    
+    private func getNowDate() -> String {
+        let now = Date()
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd hh:mm:ss"
+        return df.string(from: now)
     }
 }
 
